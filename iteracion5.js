30a478c92afe0b79a0cd00d581704660eb9fdccb
Raul <61325276+KeywizZ@users.noreply.github.com>
2022-03-01 14:25:41 +0100

    Iteracion#5

---
+++ iteracion5.js
@@ -0,0 +1,17 @@
+// Calcular promedio de strings
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(param) {
+    let sum = 0;
+    let number = 'number';
+    for (let i = 0; i < param.length; i++) {
+        if (typeof param[i] == number) {
+            sum += param[i];
+        } else {
+            sum += param[i].length;
+        }
+    }
+    return sum;
+}
+console.log(averageWord(mixedElements));
